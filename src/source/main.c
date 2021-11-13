ae1f39d5d20b27d68f175f9ae9bab14f0792ab4e
Zapharaos <mfreitag@numericable.fr>
2021-11-13 15:12:53 +0100

    comment by paul

--- src/source/main.c
+++ src/source/main.c
@@ -9,6 +9,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <pthread.h>
+#include <sys/time.h>
 
 #include "../../headers/global/utils.h"
 #include "../../headers/global/packet.h"
@@ -28,7 +29,6 @@ enum connectionStatus {
     WAITING_SYN_ACK = 0x1,
     ESTABLISHED = 0x2
 };
-
 typedef enum connectionStatus connection_status_t;
 
 enum packetStatus {
@@ -36,13 +36,13 @@ enum packetStatus {
     SEND_PACKET = 1,
     RESEND_PACKET = 2
 };
-
 typedef enum packetStatus packet_status_t;
 
 enum sendMode {
-    UNKNOWN = -1, STOP_AND_WAIT = 0, GO_BACK_N = 1
+    UNKNOWN = -1,
+    STOP_AND_WAIT = 0,
+    GO_BACK_N = 1
 };
-
 typedef enum sendMode send_mode_t;
 
 send_mode_t parseMode(char *mode) {
@@ -60,7 +60,6 @@ struct tcpSocket {
     struct sockaddr_in *sockaddr;
     connection_status_t status;
 };
-
 typedef struct tcpSocket *tcp_socket_t;
 
 /**
@@ -80,7 +79,7 @@ tcp_socket_t connectTcpSocket(char *ip, int localPort, int destinationPort) {
     sock->sockaddr = prepareSendSocket(sock->outSocket, ip, destinationPort);
 
     // On set un timeout de 500 ms
-    struct timeval tv;
+    struct timeval tv = { 0 };
     tv.tv_sec = 0;
     tv.tv_usec = TIMEOUT / 2;
 
@@ -156,7 +155,6 @@ struct flux {
     char *buf;
     int bufLen;
 };
-
 typedef struct flux *flux_t;
 
 noreturn int sendTcpSocket(tcp_socket_t socket, mode_t mode, flux_t *fluxes, int fluxCount) {
@@ -246,7 +244,6 @@ enum threadStatus {
     START = 0,
     STOP = 1
 };
-
 typedef enum threadStatus thread_status_t;
 
 // Fonction pas très safe car aucune verif de taille pour to
@@ -285,30 +282,39 @@ void *handleGoBackN(void *arg) {
 
 }
 
-
+/**
+ * Thread pour gérer un flux pour lire et renvoyer les paquets d'un flux
+ * Dans le schéma du stop and wait
+ * @param arg Structure contenant arguments
+ * @return
+ */
 void *handleStopWait(void *arg) {
     struct args args = *(struct args *) arg;
     packet_t packet = newPacket();
-    int packetsNb = args.bufLen / PACKET_DATA_SIZE;
-    int packetsDone = 0;
-    uint8_t seq = 2;
-    packet_status_t packetStatus = SEND_PACKET;
-    ssize_t ret = -1;
-    fd_set working_set;
-    char currentBuff[PACKET_DATA_SIZE];
+    int packetsNb = args.bufLen / PACKET_DATA_SIZE; // nombre de paquets à envoyer
+    int packetsDone = 0; // nombre de paquets finis
+    uint8_t seq = 2; // numéro de sequence
+    packet_status_t packetStatus = SEND_PACKET; // statut du paquet
+    ssize_t ret = -1; // retour d'erreur
+    fd_set working_set; // fd_set pour le select
+    char currentBuff[PACKET_DATA_SIZE]; // les données du paquet a envoyer actuellement
 
     // On set un timeout de 500 ms
     struct timeval tv;
 
     do {
 
-        // TIMEOUT
+        // ICI on doit gérer le 3way
+        // (pas encore fait)
+
+        // Si on attend un paquet, on rentre dans ce if
         if (packetStatus == WAIT_ACK) {
             if (ret == 0) {
-                // TIMEOUT
+                // S'il y'a un timeout alors on veut renvoyer un paquet
                 packetStatus = RESEND_PACKET;
             } else if (ret > 0) {
-                // SUCCESS
+                // S'il n'y a pas de timeout et qu'on arrive à lire un paquet
+                // Alors on lis le tube et on le traite
 
                 DEBUG_PRINT("HSW: ret > 0\n");
 
@@ -333,9 +339,16 @@ void *handleStopWait(void *arg) {
             }
         }
 
+        // Si le statut est d'envoyer un paquet,
+        // Alors on change le numéro de séquence,
+        // On y ajoute les données en question
+        // en fonction du numéro de paquet que l'on envoie
         if (packetStatus == SEND_PACKET) {
-            seq = seq == 0 ? -1 : 0;
-            int fromEnd = (packetsDone + 1) + PACKET_DATA_SIZE;
+            seq = seq == 0 ? 1 : 0;
+
+            // Ici on lit dans le buffer du flux,
+            int fromEnd = (packetsDone + 1) * PACKET_DATA_SIZE;
+
             // on vérifie que le dernier buffer n'est pas plus grand que le buffer.
             if (fromEnd > args.bufLen)
                 fromEnd = args.bufLen - fromEnd;
@@ -344,8 +357,11 @@ void *handleStopWait(void *arg) {
         }
 
         DEBUG_PRINT("HSW: Send packet fluxid=%d, status=%s\n", args.fluxId, packetStatus == SEND_PACKET ? "Send packet" : (packetStatus == RESEND_PACKET ? "Resend packet" : "Wait ACK"));
+        // On set le paquet en question,
+        // puis on l'envoie
         setPacket(packet, args.fluxId, 0, seq, 0, 0, 0, currentBuff);
         sendPacket(args.socket->outSocket, packet, args.socket->sockaddr);
+        // on met le mode du thread en WAIT ACK
         packetStatus = WAIT_ACK;
 
         FD_ZERO(&working_set);
@@ -357,44 +373,64 @@ void *handleStopWait(void *arg) {
         tv.tv_sec = 0;
         tv.tv_usec = TIMEOUT;
 
+        // On attend que l'on recoit un paquet a lire dans le tube
         ret = select(args.pipe_read + 1, &working_set, NULL, NULL, &tv);
 
         if (ret == -1) raler("select ici\n");
 
         DEBUG_PRINT("HSW: Fin boucle\n");
 
-    } while (packetsDone < packetsNb);
+    } while (1);
 
     pthread_exit(NULL);
 }
 
+/**
+ *  Cette fonction gère le thread "principal"
+ *  Ici, on recoit toutes les données du socket.
+ *  Pour chaque paquet que l'on recoit, on prend le numéro du flux
+ *  et on récupère le descripteur du tube en question.
+ *  On renvoie le paquet sur le tube trouvé précédemment,
+ *  (ce tube fait la communcation entre ce thread de gestion,
+ *  et le thread qui gère le flux du paquet).
+ * @param arg Structure contenant les descripteurs du socket, et des tubes
+ * @return
+ */
 void *handleIncomingPackets(void *arg) {
     struct incomingArgs args = *(struct incomingArgs *) arg;
     ssize_t ret;
     packet_t packet = newPacket();
 
     do {
+        // On recoit les paquets ici
         ret = recvfrom(args.socket->inSocket, packet, 52, 0, NULL, NULL);
         DEBUG_PRINT("HIP: recvfrom socket=%d\n", args.socket->inSocket);
 
+        // S'il y'a un timeout on ignore, car les timeouts sont gérés indépendamment
         if (ret < 0) // timeout
             continue;
 
         DEBUG_PRINT("HIP: no timeout\n");
 
-        if (!(packet->type & ACK))
-            continue;
+        // On vérifie ici qu'un paquet est un ACK (c'était dans le cas ou le 3Way et le 4way
+        // étaient fait pour tout les flux)
+        //if (!(packet->type & ACK))
+          //  continue;
 
+          // on vérifie si le flux existe vraiment (pour éviter les erreurs d'index)
         if (packet->idFlux >= args.fluxCount)
             continue;
 
         DEBUG_PRINT("HIP: write to flux: %d, pipe_write=%d\n", packet->idFlux, args.pipesfd[packet->idFlux][1]);
 
+        // On écrit le paquet dans le pipe du flux en question
         ret = write(args.pipesfd[packet->idFlux][1], packet, 52);
 
         if (ret < 0)
             perror("write");
 
+        // Tant que la variable partagée "threadStatus" n'est pas terminée, on continue
+        // Car dans ce thread, on ne sait pas si les flux sont tous terminés ou non.
     } while (*args.threadStatus != STOP);
 
     destroyPacket(packet);
@@ -402,32 +438,34 @@ void *handleIncomingPackets(void *arg) {
     pthread_exit(NULL);
 }
 
-
 /**
- * Démarrage les threads pour chaque flux.
+ * Démarre les threads pour chaque flux.
  * Multi-thread
  * @param socket TCP Socket
  * @param mode Mode de transmission
  * @param fluxes Tableau contenant des flux.
  * @param fluxCount Nb de flux dans le tableau
  */
-void sendTo(tcp_socket_t socket, mode_t mode, flux_t *fluxes, int fluxCount) {
+void sendTo(tcp_socket_t socket, mode_t mode, flux_t *fluxes, int fluxCount)
+{
     thread_status_t threadStatus = START;
     pthread_t *desc = malloc(sizeof(pthread_t) * (fluxCount + 1));
     struct args *table = malloc(sizeof(struct args) * fluxCount);
     int **pipes = malloc(sizeof(int) * fluxCount);
 
+    // Ici on crée la structure pour le thread de gestion
     struct incomingArgs args;
-    args.socket = socket;
-    args.fluxCount = fluxCount;
-    args.pipesfd = pipes;
-    args.threadStatus = &threadStatus;
+    args.socket = socket; // Socket receveur
+    args.fluxCount = fluxCount; // Nombre de flux
+    args.pipesfd = pipes; // Tableaux avec les descripteurs des tubes
+    args.threadStatus = &threadStatus; // Statut du thread (ne marche surement pas)
 
     // démarrer le thread de gestion
     DEBUG_PRINT("start gestion thread\n");
     pthread_create(&desc[0], NULL, handleIncomingPackets, &args);
 
     // // // //
+    // Pour chaque flux, on va créer un thread
     for (int i = 1; i <= fluxCount; ++i) {
         flux_t flux = fluxes[i - 1];
         table[i].socket = socket;
@@ -439,13 +477,19 @@ void sendTo(tcp_socket_t socket, mode_t mode, flux_t *fluxes, int fluxCount) {
         pipes[i - 1] = malloc(sizeof(int) * 2);
         DEBUG_PRINT("sendTo, fluxid=%d\n", flux->fluxId);
 
-        // OPEN PIPE
+        // On ouvre un tube pour le flux
         if (pipe(pipes[i - 1]) < 0) perror("pipe");
 
         DEBUG_PRINT("Opened new pipe for flux=%d; read=%d, write=%d\n", i - 1, pipes[i - 1][0], pipes[i - 1][1]);
 
         table[i].pipe_read = pipes[i - 1][0];
 
+
+        // Ici on va créer le thread d'un flux
+        // En fonction du mode on va démarrer une autre fonction
+        // En argument on met la structure args, qui contient
+        // Toutes les informations nécessaires à son fonctionnement
+        // Buffer, idFlux, descripteur du tube
         if (pthread_create(&desc[i], NULL, mode == STOP_AND_WAIT ? handleStopWait : handleGoBackN, &table[i]) > 0) {
             perror("pthread");
         }
@@ -459,10 +503,12 @@ void sendTo(tcp_socket_t socket, mode_t mode, flux_t *fluxes, int fluxCount) {
             perror("pthread_join");
     }
 
+    // Variable globale pour arrêter le thread de gestion
+    // Fonctionne surement pas
     threadStatus = STOP;
 
 
-    // on attend le thread de gestion
+    // on attend le thread de gestion se termine
     if (pthread_join(desc[0], NULL) > 0)
         perror("pthread_join");
 
@@ -475,129 +521,6 @@ void sendTo(tcp_socket_t socket, mode_t mode, flux_t *fluxes, int fluxCount) {
     free(pipes);
     free(table);
     free(desc);
-
-}
-
-void processus_fils(tcp_socket_t socket, flux_t flux, int pipefd[2]) {
-
-    // cb de packets il faut envoyer pour ce flux
-    int packetNb = flux->bufLen / PACKET_DATA_SIZE, currentAckNb = 0;
-    struct packet receivedPacket;
-    bool first = true;
-    struct timeval timeout;
-    timeout.tv_sec = 0;
-    timeout.tv_usec = 100;
-    fd_set working_set;
-    int ret, ecnCount = 0, timeoutCount = 0, packetsDone = 0, ackCounter = 0;
-    int congestionWindowSize = 52;
-    uint8_t lastReceivedACK = 0;
-    packet_t currentPackets[UINT8_MAX];
-
-    for (int i = 0; i < UINT8_MAX; ++i)
-        currentPackets[i] = NULL;
-
-    do {
-        // Nombre de places dans la fenêtre
-
-        if (!first) {
-
-            if (ret == 0) // TIMEOUT
-            {
-                timeoutCount++;
-            } else {
-                if (read(pipefd[0], &receivedPacket, 52) != 52)
-                    raler("read pipe");
-
-                // on attend un ack
-                if (!(receivedPacket.type & ACK))
-                    continue;
-
-                // Si l'ACQ passe cette condition alors il est valide
-                if (receivedPacket.numAcquittement == currentAckNb + 1)
-                    currentAckNb++;
-
-                // Checker s'il n'y a pas 3 fois les mêmes ACK
-                if (lastReceivedACK == receivedPacket.numAcquittement) {
-                    ackCounter++;
-
-                    if (ackCounter >= 3) {
-                        // Attention 3 ACK recu, probleme!
-                        congestionWindowSize = 52;
-
-                        // On reset tout !
-
-                        for (int i = 0; i < currentAckNb; ++i) {
-                            destroyPacket(currentPackets[i]);
-                            currentPackets[i] = NULL;
-                        }
-
-                        packetsDone += currentAckNb;
-
-                        currentAckNb = 0;
-                        timeoutCount = 0;
-                        ecnCount = 0;
-                    }
-
-                } else {
-                    ackCounter = 0;
-                    lastReceivedACK = receivedPacket.numAcquittement;
-                }
-
-                // Si bit ECN alors flux recoit fenêtre congestion plus petite
-                if (receivedPacket.ECN > 0) {
-                    ecnCount++;
-                }
-
-            }
-
-        }
-
-        int windowSize = congestionWindowSize / 52;
-
-        if (currentAckNb >= windowSize) {
-            // reinitialise tout ici
-            congestionWindowSize += 52 * currentAckNb;
-
-            for (int i = 0; i < timeoutCount; ++i)
-                congestionWindowSize /= 2;
-
-            for (int i = 0; i < ecnCount; ++i)
-                congestionWindowSize = (uint16_t) (flux->congestionWindowSize * 0.9);
-
-            for (int i = 0; i < currentAckNb; ++i) {
-                destroyPacket(currentPackets[i]);
-                currentPackets[i] = NULL;
-            }
-
-            currentAckNb = 0;
-            timeoutCount = 0;
-            ecnCount = 0;
-        } else {
-            for (int i = currentAckNb; i <= windowSize; ++i) {
-                packet_t packet = NULL;
-                if (currentPackets[i] != NULL) {
-                    packet = currentPackets[i];
-                } else {
-                    newPacket(packet);
-                    //      setPacket(packet, flux->fluxId, 0, i, 0, 0, 52, substr(flux->buf, (packetsDone + i) * 44, (packetsDone + i + 1) * 44));
-                    currentPackets[i] = packet;
-                }
-
-                sendPacket(socket->outSocket, packet, socket->sockaddr);
-            }
-        }
-
-        first = false;
-
-        FD_ZERO(&working_set);
-        FD_SET(pipefd[0], &working_set);
-
-        ret = select(pipefd[0] + 1, &working_set, NULL, NULL, &timeout);
-
-        if (ret == -1) raler("select");
-
-    } while (packetsDone < packetNb);
-
 
 }
 
@@ -645,6 +568,7 @@ int main(int argc, char *argv[]) {
 
     srand(time(NULL));
 
+    // 3way > envoi tous les flux > 4way
     tcp_socket_t sock = connectTcpSocket(ip, port_local, port_medium);
     flux_t fluxes[1];
     fluxes[0] = malloc(sizeof(struct flux));
